9aded071c3f92932426ae65d5d9ba8936289aaa5
Goernaldo <51802954+Goernaldo@users.noreply.github.com>
2026-07-16 12:46:12 +0200

    Add files via upload

---
+++ js/settings.js
@@ -0,0 +1,14 @@
+// =========================
+// Einstellungen
+// =========================
+
+window.addEventListener(
+  "load",
+  () => {
+
+    console.log(
+      "Settings geladen."
+    );
+
+  }
+);
---
+++ js/supabase.js
@@ -0,0 +1,17 @@
+// =========================
+// Supabase
+// =========================
+
+const supabaseUrl =
+  "DEINE_SUPABASE_URL";
+
+const supabaseKey =
+  "DEIN_SUPABASE_ANON_KEY";
+
+// später:
+
+// const supabase =
+// window.supabase.createClient(
+//   supabaseUrl,
+//   supabaseKey
+// );
